9c51398d135461cac319d81f9f1e47cd3ff74617
Marvin Kurland <bugemarvin@outlook.com>
2023-01-23 23:40:21 +0300

    modifying var using hoisting as undeclared variables

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    var task;
+    var task2;
+  }
+
+  return [task, task2];
+}

98dcccee90fed76588f4a9acc194aa81e4dd624a
Marvin Kurland <bugemarvin@outlook.com>
2023-01-25 16:35:42 +0300

    prototype below, return a promise. The parameter is a boolean.

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,15 @@
-export default function getFullResponseFromAPI(success){
-        return new Promise( (resolve, reject) => {
-                if (success) {
-                        resolve({status: 200, body: 'Success'});
-                } else {
-                        reject("The fake API is not working currently");
-                }
-        });
+/* eslint-disable prefer-promise-reject-errors */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    switch (success) {
+      case true:
+        resolve({ status: 200, body: 'Success' });
+        break;
+      case false:
+        reject('The fake API is not working currently');
+        break;
+      default:
+        break;
+    }
+  });
 }

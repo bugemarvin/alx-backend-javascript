f4155a88b3e87d86596164602b84246853c98ee9
Marvin Kurland <bugemarvin@outlook.com>
2023-01-25 19:30:03 +0300

    checks

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'Success' }))
+    .then(() => (
+      {
+        status: 200,
+        body: 'Success',
+      }
+    ))
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');

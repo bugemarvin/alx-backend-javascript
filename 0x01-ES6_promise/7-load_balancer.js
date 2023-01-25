6fe98a9bef713d13dffca80f8f788d575de659df
Marvin Kurland <bugemarvin@outlook.com>
2023-01-26 01:00:51 +0300

    function should return the value returned by the promise that resolved the first

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-        return Promise
-                .race([chinaDownload, USDownload])
-                .then((value) => value)
+  return Promise
+    .race([chinaDownload, USDownload])
+    .then((value) => value);
 }

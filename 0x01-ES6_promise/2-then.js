189468c801251ce9814025beece4029a6e01bc9e
Marvin Kurland <bugemarvin@outlook.com>
2023-01-25 17:43:13 +0300

    ammending using then, catch and finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    try {
-      if (promise === true) {
-        resolve({ status: 200, body: 'Success' });
-      }
-    } catch (Error) {
-      reject(Error(''));
-    } finally {
-      console.log('Got a response from the API');
-    }
-  });
+  return promise
+    .then(
+      { status: 200, body: 'Success' },
+    )
+    .catch(
+      () => Error(''),
+    )
+    .finally(
+      () => console.warn('Got a response from the API'),
+    );
 }

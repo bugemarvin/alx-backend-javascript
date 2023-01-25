8eaee073b92f4340d1483b3603267e308b7a83a3
Marvin Kurland <bugemarvin@outlook.com>
2023-01-25 19:13:36 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,8 @@ import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((txt) => console.log(`${txt[0].body} ${txt[1].firstName} ${txt[1].lastName}`))
+    .then((txt) => {
+      console.log(`${txt[0].body} ${txt[1].firstName} ${txt[1].lastName}`);
+    })
     .catch(() => console.warn('Signup system offline'));
 }

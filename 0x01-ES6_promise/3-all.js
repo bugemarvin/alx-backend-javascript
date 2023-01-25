e3081c1b281049f9fab19e16e0fbc65893b4b78c
Marvin Kurland <bugemarvin@outlook.com>
2023-01-26 01:20:56 +0300

    an async function named asyncUploadUser that will call these two functions and return an object

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/extensions */
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise

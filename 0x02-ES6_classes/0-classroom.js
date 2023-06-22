06fac1371e50f6ca1af5dd367c65030730a1d2ae
Marvin Kurland <bugemarvin@outlook.com>
2023-06-22 21:37:18 +0300

    Implement a class named ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+/**
+ * implementing a class called classRoom
+ * @param {maxStudentsSize} as an attribute of the class
+ * @param {Number} assined to @param {maxStudentsSize}
+ */
+
+export default class classRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
